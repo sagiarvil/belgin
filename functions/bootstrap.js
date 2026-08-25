c2feb959191c8ee69139ed982ede4413a228bacb
sagi arvil <teb232@gmail.com>
2026-08-26 00:12:47 +0300

    evidence: export secure legal receipt functions

--- functions/bootstrap.js
+++ functions/bootstrap.js
@@ -1,7 +1,11 @@
 const base = require('./index');
 const { completeHighValueDelivery } = require('./delivery');
+const { onOrderEvidenceFinalize, issueEvidenceAccessToken, getOrderEvidence } = require('./evidence');
 
 module.exports = {
   ...base,
   completeHighValueDelivery,
+  onOrderEvidenceFinalize,
+  issueEvidenceAccessToken,
+  getOrderEvidence,
 };
