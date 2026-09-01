3700cd83663c0dd1eda1560500b528a2426e3a4a
sagi arvil <teb232@gmail.com>
2026-09-01 15:31:42 +0300

    fix(release): remove unsupported watch claims and preserve elite brand filters

--- scratch/upgrade_iletisim_html.js
+++ scratch/upgrade_iletisim_html.js
@@ -120,7 +120,7 @@ const premiumMain = `<!-- 3. MAIN İLETİŞİM İÇERİĞİ -->
               <option>Lüks Saat Koleksiyonu İnceleme</option>
               <option>Masif Altın / Külçe & Sarrafiye Alımı</option>
               <option>Özel Tasarım Mücevher & Pırlanta</option>
-              <option>12 Nokta Ekspertiz & Değerleme Talebi</option>
+              <option>Ürün Bazında Kontrol & Değerleme Talebi</option>
               <option>Showroom Sipariş Teslimatı</option>
             </select>
           </div>
